f23b3c2a87c74f4535acf0c1c756d3c98722a9de
Josh King <windosbk+github@gmail.com>
2021-12-01 19:50:52 +1300

    (maint) Swap to Pester v5 configuration

--- .github/workflows/_pester.ps1
+++ .github/workflows/_pester.ps1
@@ -1,16 +1,21 @@
-$RelevantFiles = (Get-ChildItem './src' -Recurse -Include '*.ps1').FullName
+$PesterConfig = New-PesterConfiguration
 
-$PesterSplat = @{
-    Path                         = './test'
-    OutputFormat                 = 'JUnitXml'
-    OutputFile                   = 'TestResults.xml'
-    CodeCoverage                 = $RelevantFiles
-    CodeCoverageOutputFileFormat = 'JaCoCo'
-    CodeCoverageOutputFile       = 'CoverageResults.xml'
-    PassThru                     = $true
-}
+$PesterConfig.Run.Path = './test'
+$PesterConfig.Run.PassThru = $true
 
-$PesterResult = Invoke-Pester @PesterSplat
+$PesterConfig.TestResult.Enabled = $true
+$PesterConfig.TestResult.OutputFormat = 'JUnitXml'
+$PesterConfig.TestResult.OutputPath = 'TestResults.xml'
+
+$PesterConfig.CodeCoverage.Enabled = $true
+$PesterConfig.CodeCoverage.OutputFormat = 'JaCoCo'
+$PesterConfig.CodeCoverage.OutputPath = 'CoverageResults.xml'
+$PesterConfig.CodeCoverage.Path = './src'
+$PesterConfig.CodeCoverage.RecursePaths = $true
+
+$PesterConfig.Output.Verbosity = 'Detailed'
+
+$PesterResult = Invoke-Pester -Configuration $PesterConfig
 if ($PesterResult.FailedCount -gt 0) {
     throw "There were $($PesterResult.FailedCount) failed tests."
 }
